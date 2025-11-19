5fdf65ba59e3b559185eb0141ba4bac2f32a3c1f
ducpa-3170 <pham.anh.duc-b@sun-asterisk.com>
2025-11-19 14:31:01 +0700

    initial: Inittial project setup

---
+++ src/test/java/sun/asterisk/booking_tours/BookingToursApplicationTests.java
@@ -0,0 +1,13 @@
+package sun.asterisk.booking_tours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingToursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
