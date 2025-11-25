6d2f35218fa0b8968cbef2a7556243dfc2b7ef8f
ducpa-3170 <pham.anh.duc-b@sun-asterisk.com>
2025-11-25 18:04:30 +0700

    chore(admin): convert slug and edit placeholder total slot and available

--- src/main/resources/static/js/helpers/slug.js
+++ src/main/resources/static/js/helpers/slug.js
@@ -7,6 +7,11 @@ document.getElementById('name').addEventListener('input', function () {
 function slugify(str) {
   str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
   str = str.toLowerCase(); // convert string to lowercase
+
+  // Remove Vietnamese accents
+  str = str.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); // remove combining diacritical marks
+  str = str.replace(/đ/g, 'd').replace(/Đ/g, 'd'); // replace đ and Đ
+
   str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
     .replace(/\s+/g, '-') // replace spaces with hyphens
     .replace(/-+/g, '-'); // remove consecutive hyphens
